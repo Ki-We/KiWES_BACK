5f995cfa4e348cb2cd1eedb70d8ce90925f94e5f
linda-spr <24you@spreatics.com>
2023-09-04 20:11:18 +0900

    feat: Solve heartcontroller error

--- src/main/java/server/api/kiwes/domain/club/dto/ClubApprovalRequestSimpleDto.java
+++ src/main/java/server/api/kiwes/domain/club/dto/ClubApprovalRequestSimpleDto.java
@@ -1,14 +1,21 @@
 package server.api.kiwes.domain.club.dto;
 
 import lombok.*;
+import server.api.kiwes.domain.heart.constant.HeartStatus;
 
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @Builder
 public class ClubApprovalRequestSimpleDto {
     Long clubId;
     String title;
     Integer currentPeople;
+
+
+    public ClubApprovalRequestSimpleDto(Long clubId, String title,  Integer currentPeople) {
+        this.clubId = clubId;
+        this.title = title;
+        this.currentPeople = currentPeople;
+    }
 }
